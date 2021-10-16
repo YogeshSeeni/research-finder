ca0c1122db7d9f3c83c5c7091855c85edeaf0e96
YogeshSeeni <yogeshseeni60@gmail.com>
2021-10-15 22:15:50 -0400

    hero

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            Home Page
+            <section class="hero is-danger is-fullheight-with-navbar">
+  <div class="hero-body">
+    <div class="">
+      <p class="title">
+        Fullheight hero
+      </p>
+      <p class="subtitle">
+        Fullheight subtitle
+      </p>
+    </div>
+  </div>
+</section>
         </div>
     )
 }
