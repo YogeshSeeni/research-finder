a760be9f100b9eb68eb15c45fc9fea4c691382ec
YogeshSeeni <yogeshseeni60@gmail.com>
2021-10-15 23:39:05 -0400

    connected register page with api

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
+  const [emailText, setEmailText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
+  const [successAlert, setSuccessAlert] = useState(false);
 
-    const [emailText, setEmailText] = useState("");
-    const [passwordText, setPasswordText] = useState("");
-
-    const handleSubmit = () => {
-        console.log(emailText);
-        console.log(passwordText);
-    }
+  const handleSubmit = () => {
+    axios
+      .post("https://treasurehacks2021.pythonanywhere.com/v1/user/register", {
+        email: emailText,
+        password: passwordText,
+      })
+      .then((response) => {
+        if (response.data.success) {
+            setSuccessAlert(true)
+        }
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
-        <p class="is-size-1 has-text-centered">REGISTER</p>
+      {successAlert ? (
+        <div class="notification is-primary">
+          Registration succeeded! Please verify your email
+        </div>
+      ) : null}
+      <p class="is-size-1 has-text-centered">REGISTER</p>
       <div class="field mx-6 mt-3">
         <label class="label">Email:</label>
         <div class="control ">
@@ -22,10 +40,11 @@ export default function Register() {
             placeholder="Email input"
             value={emailText}
             onChange={(e) => {
-                setEmailText(e.target.value);
+              setEmailText(e.target.value);
             }}
           />
-        </div>      </div>
+        </div>{" "}
+      </div>
       <div class="field mx-6 mt-3">
         <label class="label">Password:</label>
         <div class="control ">
@@ -35,15 +54,16 @@ export default function Register() {
             placeholder="Password input"
             value={passwordText}
             onChange={(e) => {
-                setPasswordText(e.target.value);
+              setPasswordText(e.target.value);
             }}
           />
         </div>
       </div>
       <div class="has-text-centered">
-      <button class="button is-link mt-3" onClick={handleSubmit}>Submit</button>
+        <button class="button is-link mt-3" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
-      
     </div>
   );
 }
