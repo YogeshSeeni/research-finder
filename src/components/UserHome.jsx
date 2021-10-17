7c2b6cc2247d74bebc00554e182b8d29ac5a64ae
PhysBoom <mc_gold05@yahoo.com>
2021-10-16 20:21:15 -0700

    Added chatting

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Cookies from "universal-cookie";
 import { useHistory } from "react-router";
+import axios from 'axios';
 import Chat from './chat/Chat';
 import UserList from "./user/UserList";
 import {Box} from 'react-bulma-components';
 
-export default function UserHome() {
+export default function UserHome(props) {
   const cookies = new Cookies();
   const history = new useHistory();
+
+  const [chatId, setChatId] = useState('sadasdsadadqwdqwd');
+
+  async function handleStartChat(otherUser) {
+    const resp = await axios ({
+       method:"post",
+       url: "https://treasurehacks2021.pythonanywhere.com/v1/chat/id",
+       data: {
+         sender: cookies.get("uuid"),
+         other: otherUser
+       },
+       headers: {
+           "Authorization": cookies.get("id_token")
+       }
+
+    });
+    if (resp.data.success){
+      setChatId(resp.data.json.chat_id);
+    }
+  }
 
   if (!cookies.get("uuid")) {
     history.push("/");
@@ -15,12 +36,12 @@ export default function UserHome() {
   return (
     <article class="media">
       <div class='media-right'>
-        <UserList />
+        <UserList onInitiateChat={handleStartChat}/>
       </div>
       <div class="media-content">
         <div class="content">
           <br></br>
-          <Chat chat_id = 'test' sender={cookies.get("uuid")} id_token={cookies.get("id_token")}/>
+          <Chat chat_id = {chatId} sender={cookies.get("uuid")} id_token={cookies.get("id_token")}/>
         </div>
         <nav class="level is-mobile">
           <div class="level-left">
